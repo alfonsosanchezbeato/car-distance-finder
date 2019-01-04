70eaa1b463c36bd4bce6a5986ea2e7edba9bca75
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-01-04 13:20:33 +0100

    Add class for tracked vehicles

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import argparse
 import cv2
 import numpy as np
 import sys
+from vehicle import TrackedVehicle, BoundingBox
 
 PROGRAM = 'detector'
 VERSION = '0.1'
@@ -20,14 +21,6 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
            "sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
-
-
-class bounding_box:
-    def __init__(self, top_x, top_y, bottom_x, bottom_y):
-        self.top_x = 0
-        self.top_y = 0
-        self.bottom_x = 0
-        self.bottom_y = 0
 
 
 def load_model():
@@ -63,14 +56,19 @@ def detect_vehicle(net, min_confidence, frame):
         if confidence < min_confidence:
             continue
 
-        # extract the index of the class label from the detections
-        # then compute the (x, y)-coordinates of the bounding box for
-        # the object
+        # Get the index of the class label from the detections and filter
+        # non-vehicles
         idx = int(detections[0, 0, i, 1])
+        if CLASSES[idx] != "bicycle" and CLASSES[idx] != "bus" and \
+           CLASSES[idx] != "car" and CLASSES[idx] != "motorbike":
+            continue
+
+        # Get bounding box for the object
         print(detections[0, 0, i, 3:7])
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
-        vehicles.append(bounding_box(startX, startY, endX, endY))
+        vehicles.append(BoundingBox(startX, startY,
+                                    endX - startX, endY - startY))
 
         # display the prediction
         label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
@@ -86,8 +84,27 @@ def detect_vehicle(net, min_confidence, frame):
 
 # Merges currently tracked vehicles with currently detected. This can only add
 # vehicles to the list - they get removed only when tracking fails.
-def merge_vehicles(tracked_vehicles, vehicles):
-    return
+def merge_vehicles(frame, vehicles, tracked_vehicles):
+    # XXX just track the first set for the moment
+    if tracked_vehicles:
+        return
+    for v in vehicles:
+        tracked_vehicles.append(TrackedVehicle(frame, v))
+
+
+def tracker_update(frame, vehicle):
+    (success, box) = vehicle.update(frame)
+    if success:
+        box = vehicle.roi
+        color = (0, 0, 255)
+        cv2.rectangle(frame, (box.top_x, box.top_y),
+                      (box.top_x + box.width, box.top_y + box.height),
+                      color, 2)
+        label = "Distance: {:.2f} m".format(vehicle.calculate_distance())
+        y = box.top_y - 15 if box.top_y - 15 > 15 else box.top_y + 15
+        cv2.putText(frame, label, (box.top_x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    return success
 
 
 def frame_loop(video, net):
@@ -100,16 +117,21 @@ def frame_loop(video, net):
         if res is False:
             break
         # XXX make confidence configurable?
-        vehicles = detect_vehicle(net, 0.2, frame)
+        if not tracked_vehicles:
+            print('Detecting vehicles')
+            vehicles = detect_vehicle(net, 0.2, frame)
+        merge_vehicles(frame, vehicles, tracked_vehicles)
+        # Track and show currently detected vehicles
+        tracked_vehicles[:] = [v for v in tracked_vehicles
+                               if tracker_update(frame, v)]
         cv2.imshow("Press 'q' to exit", frame)
         k = cv2.waitKey(0)
         if k == ord('q'):
             break
-        merge_vehicles(tracked_vehicles, vehicles)
-        # Track currently detected vehicles
 
 
 def main(argv=None):
+    print('CV version is', cv2.__version__)
     if argv is None:
         argv = sys.argv[1:]
     args = parseargs(argv)
---
+++ vehicle.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2018 Alfonso Sanchez-Beato
+
+import cv2
+
+
+class BoundingBox:
+    def __init__(self, top_x, top_y, width, height):
+        self.top_x = int(top_x)
+        self.top_y = int(top_y)
+        self.width = int(width)
+        self.height = int(height)
+
+
+class TrackedVehicle:
+    # Receives a frame and a bounding box with the current vehicle position
+    def __init__(self, frame, roi):
+        self.roi = roi
+        # The way to do tracker initialization depends on the openCV version
+        (major, minor) = cv2.__version__.split(".")[:2]
+        if int(major) == 3 and int(minor) < 3:
+            self.tracker = cv2.Tracker_create("KCF")
+        else:
+            self.tracker = cv2.TrackerKCF_create()
+        self.tracker.init(frame, (roi.top_x, roi.top_y, roi.width, roi.height))
+        # XXX Constants for the moment
+        # Focal length (pix)
+        self._focal_len = 300
+        # Lenght of back part of a vehicle (meters). Should change depending on
+        # the vehicle type.
+        self._vehicle_back_len = 2
+
+    def update(self, frame):
+        (success, box) = self.tracker.update(frame)
+        self.roi = BoundingBox(*box)
+        return success, self.roi
+
+    def calculate_distance(self):
+        return self._vehicle_back_len*self._focal_len/self.roi.width
